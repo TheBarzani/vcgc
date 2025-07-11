45dcc9a5234b8955190c801c6a36216476b6a026
TheBarzani <ismaelmergasori@gmail.com>
2025-07-11 10:33:44 -0400

    Migrate to Qiskit 2.0 and new IBM Cloud platform

--- playground.py
+++ playground.py
@@ -1,5 +1,13 @@
 from vcgc.network import *
+from networkx import bfs_tree
+import matplotlib.pyplot as plt
+import networkx as nx
 
 G = VCPNetwork()
-G.read_dimacs("dimacs/g000.dimacs")
+G.read_dimacs("dimacs/g002.dimacs")
 G.draw_colored_graph()
+T = bfs_tree(G.graph, source=0)
+plt.figure(figsize=[10,10])
+nx.draw(T)
+plt.savefig('tree.png', dpi=300, bbox_inches='tight')
+print(T.edges())
